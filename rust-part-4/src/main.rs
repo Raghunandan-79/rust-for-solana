e30cc91bd192b85f829ba14f242027e8fad16809
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-08-28 11:54:27 +0530

    Macros deep dive and rust part 4 completed

--- rust-part-4/src/main.rs
+++ rust-part-4/src/main.rs
@@ -1,4 +1,6 @@
-use std::f32::consts::PI;
+use std::{f32::consts::PI, fmt::Display};
+
+use serde::{Deserialize, Serialize};
 
 trait Shape {
     fn area(&self) -> f32;
@@ -53,8 +55,85 @@ fn traits() {
     println!("Perimeter of circle is: {}", get_perimeter_and_area(&circle).1);
 }
 
-fn macros() {
+macro_rules! say_hello {
+    () => {
+        println!("Hello world!");
+    };
+}
 
+// Defining a create_funtion macro
+macro_rules! create_funtion {
+    ($func_name:ident) => {
+        fn $func_name() {
+            println!("Hello from {}", stringify!($func_name));
+        }
+    };
+}
+
+create_funtion!(hello);
+
+// procedural macros
+#[derive(Debug, PartialEq)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Display for Rectangle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Rectangle has a width of {}, and height of {}", self.width, self.height)
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    #[serde(rename = "user_name")]
+    username: String,
+
+    #[serde(rename = "pass_word")]
+    password: String,
+
+    #[serde(rename = "user_age")]
+    age: u32
+}
+
+fn macros_applied_to_attributes() {
+    let user: User = User { 
+        username: String::from("Alice"), 
+        password: String::from("password123"), 
+        age: 30, 
+    };
+
+    let json: String = serde_json::to_string(&user).unwrap();
+    println!("{}", json);
+}
+
+fn macros() {
+    say_hello!();
+    hello();
+
+    let rect: Rectangle = Rectangle { 
+        width: 10, 
+        height: 30 
+    };
+    println!("{:?}", rect);
+    println!("Width: {}", rect.width);
+    println!("Height: {}", rect.height);
+
+    let rect2: Rectangle = Rectangle {
+        width: 10, 
+        height: 40 
+    };
+    
+    if rect == rect2 {
+        println!("They are equal");
+    } else {
+        println!("They are not equal");
+    }
+
+    println!("{}", rect);
+
+    macros_applied_to_attributes();
 }
 
 fn main() {
